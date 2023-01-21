e82d24bc4e80be0d029af21d88ee31fee506b6f9
iMuhaBook <maximusmuha2121@gmail.com>
2023-01-21 16:54:48 +0300

    Commiting main test

--- folderMuha/MyTestingJavaFile/src/Main.java
+++ folderMuha/MyTestingJavaFile/src/Main.java
@@ -1,9 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        int a, b;
-        a = 1;
-        b = 2;
-        int res = a+b;
-        System.out.println("Summary " + a + " + " +  b + " = " + res);
+        String result;
+        String text;
+        int counter, randomValue;
+        try {
+            FileReader fl = new FileReader("1.txt");
+            BufferedReader in = new BufferedReader(file);
+            String str;
+            while ((str = in.readLine()) != null) {
+                text = str;
+                if (counter == randomValue) {
+                    result = text;
+                }
+                counter++;
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
